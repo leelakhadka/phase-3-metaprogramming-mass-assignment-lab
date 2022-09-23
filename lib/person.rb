a9aabba6b0810c3b57a85ec16476dc5f5f35e536
leela khadka <khadkaleela3@gmail.com>
2022-09-22 21:09:13 -0700

    commit tree 5b8f09dea9d62dc53cd8cbd91da6ea916c65aae8

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
 class Person
-  # your code here
+  avi.attributes = {name: birthday: hair_color: eye_color: height:
+  weight: handed: complexion: t_shirt_size: wrist_size: glove_size:
+  pant_length: pant_width: }
+      end
+    
+  
+  
+  
+
+
 end
